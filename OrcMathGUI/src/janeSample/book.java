111442565a57f64bb9f8029bc4199aa03ec1661c
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-12-14 11:45:27 -0500

    srtsd

---
+++ OrcMathGUI/src/janeSample/book.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package janeSample;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import guiTeacher.components.Component;
+
+/**
+ * @author BT_1N3_02
+ *
+ */
+public class book extends Component {
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param w
+	 * @param h
+	 */
+	public book() {
+		super(40, 40, 100, 100);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see guiTeacher.components.Component#update(java.awt.Graphics2D)
+	 */
+	@Override
+	public void update(Graphics2D g) {
+		g.setColor(Color.BLUE);
+		g.drawRect(0, 0, getWidth(), getHeight());
+
+	}
+
+}
