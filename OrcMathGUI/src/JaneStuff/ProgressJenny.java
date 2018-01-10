cd3bde57c67f386d4e77b5346b2da74d6ac82177
jfeng5529 <31857091+jfeng5529@users.noreply.github.com>
2018-01-10 09:51:45 -0500

    Update ProgressJenny.java
    Sjs

--- OrcMathGUI/src/JaneStuff/ProgressJenny.java
+++ OrcMathGUI/src/JaneStuff/ProgressJenny.java
@@ -1,31 +1,54 @@
 package JaneStuff;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import guiTeacher.components.Component;
 
-public class ProgressJenny extends Component implements ProgressInterfaceJane {
-	
-	private int size;
+public class ProgressJenny extends Component implements ProgressInterfaceJane{
+
 	private int round;
-
-	public ProgressJenny(int x, int y, int w, int h) {
+	private int sequenceSize;
+	private boolean progress;
+	
+	public ProgressJane(int x, int y, int w, int h) {
 		super(x, y, w, h);
+		progress = true;
+		update();
 	}
 
+	@Override
 	public void gameOver() {
-		
+		progress = false;
+		update();
 	}
 
+	@Override
 	public void setRound(int roundNumber) {
 		round = roundNumber;
+		update();
 	}
 
+	@Override
 	public void setSequenceSize(int size) {
-		this.size = size;
+		sequenceSize = size;
+		update();
 	}
 
+	@Override
 	public void update(Graphics2D g) {
+		clear();
+		if(progress) {
+			g.setColor(Color.black);
+			g.drawString("Round: "+round, 600, 50);
+			g.drawString("Sequence: "+sequenceSize,600,100);
+		}else {
+			g.setColor(Color.black);
+			g.drawString("Game over", 600, 50);
+			g.drawString("Round: "+round, 600, 100);
+			g.drawString("Sequence: "+sequenceSize,600,150);
+		}
 
 	}
+
 }
